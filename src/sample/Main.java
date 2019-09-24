e5e1570313589cef80bab8dc817e3966ee07924a
Sindre Haavaldsen <sindre.h@outlook.com>
2019-09-24 17:12:30 +0200

    Ferdig

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,20 +4,56 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
+import static javax.swing.JOptionPane.*;
 
 public class Main extends Application {
 
+    Pane canvas;
+    private int screenWidth = 800;
+    private int screenHeight = 800;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        String melding = showInputDialog("Velg");
+        BorderPane pane = new BorderPane();
+        canvas = new Pane();
+        pane.setCenter(canvas);
+        branch(400, 500, -Math.PI / 2, 8);
+        primaryStage.setTitle("Algo oblig 1");
+        primaryStage.setScene(new Scene(pane, screenWidth, screenHeight));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public void branch(int startX, int startY, double vinkel, int lengde) {
+        if (lengde == 0) {
+            return;
+        }
+        int endX = startX + (int) (Math.cos(vinkel) * lengde * 10);
+        int endY = startY + (int) (Math.sin(vinkel) * lengde * 10);
+
+        canvas.getChildren().add(
+                new Line(startX, startY, endX, endY)
+        );
+
+        branch(endX, endY, vinkel - Math.PI / 8, lengde - 1);
+        branch(endX, endY, vinkel + Math.PI / 8, lengde - 1);
+        /*if (lengde == 0) {
+            return;
+        }
+        int endX = startX + (int) (Math.cos(Math.toRadians((vinkel)) * lengde * 10));
+        int endY = startY + (int) (Math.sin(Math.toRadians((vinkel)) * lengde * 10));
+        Line linje = new Line(startX, startY, endX, endY);
+        canvas.getChildren().add(linje);
+        branch(endX, endY, vinkel-20, lengde-1);
+        branch(endX, endY, vinkel+20, lengde-1);*/
     }
 }
